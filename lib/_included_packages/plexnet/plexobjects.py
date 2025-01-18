ab9d830414a91731075876633baa48d303853746
pannal <1359593+pannal@users.noreply.github.com>
2025-01-18 22:46:49 +0100

    Core: Revert "Core/PlexObjects: Don't store original element data to reduce memory usage (wasn't used anyways)"; fix regression in library folder view

--- lib/_included_packages/plexnet/plexobjects.py
+++ lib/_included_packages/plexnet/plexobjects.py
@@ -175,6 +175,7 @@ class PlexObject(Checks):
         self.titleSort = PlexValue('')
         self.deleted = False
         self._reloaded = False
+        self.data = data
 
         if data is None:
             return
